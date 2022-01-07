98b842f35218b7b69e063f2e1eb18a367c51d74b
Dmitry <example@gmail.com>
2022-01-07 21:15:17 +0300

    create apps

---
+++ js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+
+// const answer = +prompt('Вам есть 18?', '18');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Вам есть 18?', '');
+// answers[1] = prompt('Как ваше имя', '');
+// answers[2] = prompt('Сколько вам лет', '');
+
+// document.write(answers);qwe
+
+//Интерполяция:
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Dmitry';
+
+// alert(`Привет, ${user}`);
+
+//Операторы JS:
+
+// let incr = 10,
+//     decr = 10;
+
+// ++incr;
+// --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+const isCheck = false;
+      isClose = false;
+
+console.log(isCheck ||  !isClose);
+
