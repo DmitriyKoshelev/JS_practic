80c16d89a98e40f7045e8cefb78d50397c24d02c
Dmitry <example@gmail.com>
2022-01-09 20:53:34 +0300

    overwrite function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+'use strict';
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько сериалов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько сериалов вы уже посмотрели?', '');        
+    }
+}
+
+start();
+
+
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,28 +22,50 @@ const personalMovieDB = {
     privat: false        
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних простотренных фильмов?', ''),
-          b = prompt('На сколько оценили его?', '');
-    if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
-        personalMovieDB.movies [a] = b;
-        console.log('Done');
-    }  else {
-        console.log('Error');
-        i--;
-    }    
-          
+function rememberFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных сериалов?', ''),
+              b = prompt('На сколько оценили его?', '');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+            personalMovieDB.movies [a] = b;
+            console.log('Done');
+        }  else {
+            console.log('Error');
+            i--;
+        }    
+              
+    }   
+}
+     rememberFilms();
+
+function personalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотренно мало сериалов');
+    } else if (personalMovieDB.count > 10 &&  personalMovieDB.count < 30) {
+        console.log('Вы классический любитель');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Повелитель сериалов');
+    } else {
+        console.log('Ошибка');
+    }
+    
 }
 
-if (personalMovieDB.count < 10) {
-    console.log('Просмотренно мало сериалов');
-} else if (personalMovieDB.count > 10 &&  personalMovieDB.count < 30) {
-    console.log('Вы классический любитель');
-} else if (personalMovieDB.count >= 30) {
-    console.log('Повелитель сериалов');
-} else {
-    console.log('Ошибка');
+ personalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }   
 }
 
-console.log(personalMovieDB);
+showMyDB(personalMovieDB.privat);
+
+function writeGeners() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }   
+}
+
+writeGeners();
 
